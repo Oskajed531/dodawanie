8328aeb1f8f6d0032d5b4493b7ccad01a9c255df
Oskajed531 <72351667+Oskajed531@users.noreply.github.com>
2023-04-18 16:59:39 +0200

    Add files via upload

---
+++ src/test/java/pl/example/dodawanie/DodawanieApplicationTest.java
@@ -0,0 +1,12 @@
+package pl.example.dodawanie;
+
+import static org.junit.Assert.*;
+
+public class DodawanieApplicationTest {
+
+    @org.junit.Test
+    public void added() {
+        DodawanieApplication dodawanieApplication = new DodawanieApplication();
+        assertEquals(10,dodawanieApplication.added(5,5));
+    }
+}
